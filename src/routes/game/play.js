a08521c99a4d8e1706b3209f5fd39dcd736a8f7b
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-05-25 09:49:21 +0100

    make board resize itself

--- src/routes/game/play.js
+++ src/routes/game/play.js
@@ -1,11 +1,26 @@
 import { Chessboard } from "react-chessboard";
 
 export default function Play() {
+  const [chessboardSize, setChessboardSize] = useState(undefined);
+  const [selectedBoard, setSelectedBoard] = useState('chessBoard');
+
+  // https://github.com/Clariity/react-chessboard/blob/main/example/src/index.js
+  useEffect(() => {
+      function handleResize() {
+        const display = document.getElementsByClassName('container')[0];
+        setChessboardSize(display.offsetWidth - 20);
+      }
+
+      window.addEventListener('resize', handleResize);
+      handleResize();
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Play</h2>
-        <div>
-          <Chessboard id="BasicBoard" />
+        <div className="container">
+          <Chessboard id="BasicBoard" boardWidth={chessboardSize} />
         </div>
       </main>
     );
