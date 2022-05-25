655f1df6d66bd026cf9a27c9988e0715616a7602
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-05-25 09:22:36 +0100

    fingers crossed!

--- src/routes/game/play.js
+++ src/routes/game/play.js
@@ -1,7 +1,12 @@
+import { Chessboard } from "react-chessboard";
+
 export default function Play() {
     return (
       <main style={{ padding: "1rem 0" }}>
         <h2>Play</h2>
+        <div>
+          <Chessboard id="BasicBoard" />
+        </div>
       </main>
     );
   }
