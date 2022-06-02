0010e49c0eac01a39ca52a0fd3d0e9564f2fbfa4
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-02 12:43:16 +0000

    test updater

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,6 +4,7 @@ export default function Home() {
       <main style={{ padding: "1rem 0" }}>
         <h2>Welcome to Checkmate</h2>
         <p>This area is under construction. Check back soon!</p>
+        <p>In the future, this website will offer many ways to play chess.</p>
       </main>
     );
   }
