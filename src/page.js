a8d92485a2b15f0c01cc8aee5929e5424fdc02f3
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-03 17:14:53 +0000

    Drop "Checkmate" in doc title if in pwa

--- src/page.js
+++ src/page.js
@@ -1,8 +1,15 @@
 import { useEffect } from "react";
 
+// https://stackoverflow.com/a/52695341
+const isInStandaloneMode = () =>
+      (window.matchMedia('(display-mode: standalone)').matches) || (window.navigator.standalone) || document.referrer.includes('android-app://');
+
 const Page = (props) => {
   useEffect(() => {
-    document.title = (props.title || "") + " | Checkmate";
+    document.title = (props.title || "");
+    if (!isInStandaloneMode()) {
+      document.title += " | Checkmate";
+    }
   }, [props.title]);
   return props.children;
 };
