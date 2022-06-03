d7ea82d21c358404b369a51920b42005bf74ff7c
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-03 18:04:33 +0000

    adapt PlayRandomMove (to use for making my own)

--- src/boards/PlayRandomMove.js
+++ src/boards/PlayRandomMove.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Chess from "chess.js";
+import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 
 export default function PlayRandomMoveEngine() {
