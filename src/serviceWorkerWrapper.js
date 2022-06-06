3f40d308cb728713e902801c0c59883ba896a720
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-06 18:13:56 +0000

    turn serviceWorkerWrapper into a PureComponent

--- src/serviceWorkerWrapper.js
+++ src/serviceWorkerWrapper.js
@@ -2,70 +2,78 @@
 // needed conversion from typescript
 // also needed to make service worker registration work
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
 import * as serviceWorker from './serviceWorkerRegistration';
 
-const ServiceWorkerWrapper = () => {
-  const [isOnline, setOnline] = useState(navigator.onLine);
+class ServiceWorkerWrapper extends React.PureComponent {
+  constructor(props) {
+    super(props);
 
-  function updateOnlineStatus(event) {
-    setOnline(navigator.onLine);
+    this.state = {
+      online: navigator.onLine,
+      showReload: true,
+      waitingWorker: null
+    };
+
+    window.addEventListener('online',  this.updateOnlineStatus);
+    window.addEventListener('offline', this.updateOnlineStatus);
+
+    serviceWorker.register({ onUpdate: this.onSWUpdate });
   }
 
-  window.addEventListener('online',  updateOnlineStatus);
-  window.addEventListener('offline', updateOnlineStatus);
+  updateOnlineStatus = (event) => {
+    this.setState({ online : navigator.onLine });
+  }
 
-  const [showReload, setShowReload] = React.useState(false);
-  const [waitingWorker, setWaitingWorker] = React.useState(null);
-
-  const onSWUpdate = (registration) => {
-    setShowReload(true);
-    setWaitingWorker(registration.waiting);
+  onSWUpdate = (registration) => {
+    this.setState({ showReload: true });
+    this.waitingWorker = registration.waiting;
     console.log(registration.active);
   };
 
-  useEffect(() => {
-    serviceWorker.register({ onUpdate: onSWUpdate });
-  }, []);
+  componentDidMount() {
+  }
 
-  const reloadPage = () => {
-    waitingWorker?.postMessage({ type: 'SKIP_WAITING' });
-    setShowReload(false);
+  reloadPage = () => {
+    this.state.waitingWorker?.postMessage({ type: 'SKIP_WAITING' });
+    this.handleClose()
     window.location.reload(true);
   };
 
-  const handleClose = () => setShowReload(false);
+  handleClose = () => { this.setState({ showReload: false })};
 
-  return (
-    <Modal
-      show={showReload}
-      onHide={handleClose}
-      backdrop="static"
-      keyboard={false}
-    >
-        <Modal.Header closeButton>
-            <Modal.Title>Update</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>An update is available for Checkmate.</Modal.Body>
-        <Modal.Footer>
-            <Button
-              variant="secondary"
-              onClick={handleClose}
-            >
-              Dismiss
-            </Button>
-            <Button
-              variant="primary"
-              onClick={isOnline? reloadPage: null}
-              disabled={!isOnline}
-            >
-              {isOnline ? 'Update' : 'Offline'}
-            </Button>
-        </Modal.Footer>
-    </Modal>
-  )
+  render() {
+    return (
+      <Modal
+        show={ this.state.showReload }
+        onHide={ this.handleClose }
+        backdrop="static"
+        keyboard={false}
+      >
+          <Modal.Header closeButton>
+              <Modal.Title>Update</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>An update is available for Checkmate.</Modal.Body>
+          <Modal.Footer>
+              <Button
+                variant="secondary"
+                onClick={this.handleClose}
+              >
+                Dismiss
+              </Button>
+              <Button
+                variant="primary"
+                onClick={this.state.online ? this.reloadPage : null}
+                disabled={!this.state.online}
+              >
+                {this.state.online ? 'Update' : 'Offline'}
+              </Button>
+          </Modal.Footer>
+      </Modal>
+    )
+  }
 }
 
 export default ServiceWorkerWrapper;
