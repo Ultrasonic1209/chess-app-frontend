4cdf92830286692d87f936128cdf08d5bca0c11b
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-07 22:25:25 +0100

    coma
    With an extra m somewhere

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = async (phase, { defaultConfig }) => {
      * @type {import('next').NextConfig}
      */
     const nextConfig = {
-        legacyBehavior: false
-        distDir: 'build',
+        legacyBehavior: false,
+        distDir: 'build'
     }
     return nextConfig
   }
