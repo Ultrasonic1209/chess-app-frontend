c6ff92e23a395d99d42bd21b1ddaa200f2aed892
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-08 21:00:25 +0000

    more tags!

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const APP_NAME = 'Checkmate'
+
 export default function Document() {
   return (
     <Html lang='en'>
@@ -9,13 +11,16 @@ export default function Document() {
           name="description"
           content="A webapp where you can play chess."
         />
+        <meta name='format-detection' content='telephone=no' />
+        <meta name='mobile-web-app-capable' content='yes' />
         <meta name="color-scheme" content="light dark" />
         <link rel="apple-touch-icon" sizes="180x180" href={process.env.PUBLIC_URL + "/apple-touch-icon.png"} />
         <link rel="icon" type="image/png" sizes="32x32" href={process.env.PUBLIC_URL + "/favicon-32x32.png"} />
         <link rel="icon" type="image/png" sizes="16x16" href={process.env.PUBLIC_URL + "/favicon-16x16.png"} />
         <link rel="mask-icon" href={process.env.PUBLIC_URL + "/safari-pinned-tab.svg"} color="#000000" />
-        <meta name="apple-mobile-web-app-title" content="Checkmate" />
-        <meta name="application-name" content="Checkmate" />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="application-name" content={APP_NAME} />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)" />
         <meta name="theme-color" content="#3b3b3b" media="(prefers-color-scheme: dark)" />
