c37fa1cec94cc4ac0f97489d516e1abc0af32021
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-08 20:37:55 +0000

    testin something

--- next.config.js
+++ next.config.js
@@ -1,11 +1,26 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
 module.exports = async (phase, { defaultConfig }) => {
     /**
      * @type {import('next').NextConfig}
      */
     const nextConfig = {
+        reactStrictMode: true,
         experimental: {
             newNextLinkBehavior: true /* this is not documented AT ALL. */
+        },
+        env: {}
+    }
+
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        nextConfig.env = {
+            PUBLIC_URL: "http://127.0.0.1:3000/"
+        }
+    } else {
+        nextConfig.env = {
+            PUBLIC_URL: process.env.CF_PAGES_URL
         }
     }
+
     return nextConfig
   }
