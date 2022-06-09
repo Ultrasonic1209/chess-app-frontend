2503e5d915ed72ad4c0ba6401625245d3e58bc04
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-09 21:28:23 +0000

    oh my days

--- components/sw-wrapper.js
+++ components/sw-wrapper.js
@@ -45,7 +45,9 @@ const SWWrapper = () => {
       // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
       // NOTE: MUST set skipWaiting to false in next.config.js pwa object
       // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
-      const promptNewVersionAvailable = event => {
+
+      // eslint-disable-next-line no-unused-vars
+      const promptNewVersionAvailable = (event) => {
         // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
         // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
         // You may want to customize the UI prompt accordingly.
