9332371aae6a90801a0f0df826cb6c7997ecd10e
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-09 21:05:58 +0000

    add dev tag

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Main from "../components/main";
 
+const IS_DEV = process.env.NEXT_PUBLIC_IS_VERCEL;
+
 export default function Home() {
     return (
       <Main title="Home">
         <h2>Welcome to Checkmate</h2>
         <p>This area is under construction. Check back soon!</p>
         <p>In the future, this website will offer many different ways to play chess.</p>
+        {IS_DEV ? <p><b>You are currently accessing a developer build of Checkmate. Things may break.</b></p> : ''}
       </Main>
     );
   }
