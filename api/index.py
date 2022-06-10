a3ce1d1310ce3adfb9c13ff14da8fa2cec697446
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-10 16:24:33 +0000

    ?????????

--- api/index.py
+++ api/index.py
@@ -4,19 +4,15 @@ from sanic.response import json
 
 from sanic_ext import Config
 
-from a2wsgi import ASGIMiddleware
+app = Sanic("CheckmateBackend")
 
-api = Sanic("CheckmateBackend")
-
-app = ASGIMiddleware(api)
-
-api.extend(config=Config(
+app.extend(config=Config(
     oas=True,
     oas_autodoc=True,
     oas_ui_default="swagger"
 ))
 
-@api.middleware('response')
+@app.middleware('response')
 async def add_json(request: sanic.Request, response: sanic.response.HTTPResponse):
     if response.content_type == "application/json":
         parsed = ujson.loads(response.body)
@@ -29,10 +25,11 @@ async def add_json(request: sanic.Request, response: sanic.response.HTTPResponse
     else:
         return None
 
-@api.get("/")
-@api.route('/<path:path>',methods=["GET","POST"])
+@app.get("/")
+@app.route('/<path:path>')
 async def index(request, path=""):
     return json({"message": "Hello, world.", "path": path})
 
+
 if __name__ == '__main__':
-    api.run(host='0.0.0.0', port=8080, fast=True)
+    app.run(host='0.0.0.0', port=8080, fast=True)
