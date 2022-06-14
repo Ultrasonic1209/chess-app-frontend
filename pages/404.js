c3632d2f313e817281929337618345d5fff5feb3
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-14 09:50:10 +0000

    dynamic optimisatioN!!!

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Main from "../components/main";
+
+export default function NotFound() {
+    return (
+      <Main title="404">
+        <h2>HTTP 404</h2>
+        <p>Page not found.</p>
+      </Main>
+    );
+  } 
---
+++ pages/500.js
@@ -0,0 +1,10 @@
+import Main from "../components/main";
+
+export default function ServerError() {
+    return (
+      <Main title="404">
+        <h2>HTTP 500</h2>
+        <p>Internal server error.</p>
+      </Main>
+    );
+  } 
