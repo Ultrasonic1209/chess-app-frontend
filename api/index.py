e3f283377fbe3a7386d8bc0d7f64cc4fd4468987
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-15 09:01:16 +0000

    its jsut broken smh

--- api/index.py
+++ api/index.py
@@ -1,9 +1,36 @@
+import sanic, sanic.response, ujson
 from sanic import Sanic
 from sanic.response import json
-app = Sanic(name="CheckmateBackend")
 
+from sanic_ext import Config
 
-@app.route('/')
+app = Sanic("CheckmateBackend")
+
+app.extend(config=Config(
+    oas=True,
+    oas_autodoc=True,
+    oas_ui_default="swagger"
+))
+
+@app.middleware('response')
+async def add_json(request: sanic.Request, response: sanic.response.HTTPResponse):
+    if response.content_type == "application/json":
+        parsed = ujson.loads(response.body)
+
+        parsed["chess"] = "cool"
+
+        new_response = json(parsed, status=response.status, headers=response.headers)
+
+        return new_response
+    else:
+        return None
+
+@app.get("/")
 @app.route('/<path:path>')
 async def index(request, path=""):
-    return json({'hello': path})
+    return json({"message": "Hello, world.", "path": path})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, fast=True)
+
