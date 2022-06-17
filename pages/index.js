a7ec5e3544a95460fdd467e7808ce075a67e7b13
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-06-17 12:50:03 +0000

    canonical moment

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
+import Head from "next/head";
 import Main from "../components/main";
 
 const IS_DEV = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview";
 
 export default function Home() {
     return (
-      <Main title="Home">
-        <h2>Welcome to Checkmate</h2>
-        <p>This area is under construction. Check back soon!</p>
-        <p>In the future, this website will offer many different ways to play chess.</p>
-        {IS_DEV ? <p><b>You are currently accessing a developer build of Checkmate. Things may break.</b></p> : ''}
-      </Main>
+      <>
+        <Head>
+          <link rel="canonical" href={IS_DEV ? 'https://dev.chessapp.ultras-playroom.xyz' : 'https://chessapp.ultras-playroom.xyz'} />
+        </Head>
+        <Main title="Home">
+          <h2>Welcome to Checkmate</h2>
+          <p>This area is under construction. Check back soon!</p>
+          <p>In the future, this website will offer many different ways to play chess.</p>
+          {IS_DEV ? <p><b>You are currently accessing a developer build of Checkmate. Things may break.</b></p> : ''}
+        </Main>
+      </>
     );
   }
