d8178b3d5794f0794a26af09c156a474ba99a034
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-07-29 11:41:36 +0000

    bugfix

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,9 +1,11 @@
 // https://nextjs.org/docs/basic-features/data-fetching/client-side
+import { useRouter } from 'next/router'
 import Main from "../../components/Main";
 import useSWR from 'swr'
 
 const fetcher = url => fetch(url, {withCredentials: true, credentials: 'include'}).then(r => r.json())
 export default function Profile() {
+    const router = useRouter();
     const { data, error } = useSWR('https://apichessapp.server.ultras-playroom.xyz/login/identify', fetcher)
 
     if (error) {
@@ -23,10 +25,9 @@ export default function Profile() {
       )
     }
     else if (!data.name) {
+      router.push("/sign-in")
       return (
         <Main title="Profile">
-          <h2>Profile</h2>
-          <p>Loading...</p>
         </Main>
       )
     }
@@ -34,7 +35,7 @@ export default function Profile() {
       return (
         <Main title="Profile">
           <h2>Profile</h2>
-          <p>Username: {data.username}</p>
+          <p>Username: {data.name}</p>
         </Main>
       );
     }
