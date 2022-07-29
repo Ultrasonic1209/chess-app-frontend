259742d248f9c80f86470d4cc92f1702517e882f
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-07-29 22:11:42 +0000

    disable workbox logging if not in dev

--- worker/index.js
+++ worker/index.js
@@ -1,7 +1,9 @@
+const IS_DEV = process.env.NEXT_PUBLIC_VERCEL_ENV === "preview";
+
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
 //
-// self.__WB_DISABLE_DEV_LOGS = true
+self.__WB_DISABLE_DEV_LOGS = !IS_DEV
 
 // listen to message event from window
 self.addEventListener('message', event => {
