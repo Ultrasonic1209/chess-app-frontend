8b7215039204273e3c30f0812b36851557fd4641
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-07-31 11:49:34 +0000

    performance enhancements to /game/new

--- pages/game/new.js
+++ pages/game/new.js
@@ -12,14 +12,16 @@ export default function Play() {
   // https://github.com/Clariity/react-chessboard/blob/main/example/src/index.js
   useEffect(() => {
       function handleResize() {
-        const display = document.getElementsByClassName('container')[0];
-        var size = display.offsetWidth
-        if (size >= 720) { // desktop/tablet
-          size /= 2.15; // chess board on one side, list of moves on the other
-        } else if (size <= 575) { // phone
-          size -= 50;
-        }
-        setChessboardSize(size);
+        setTimeout(() => {
+          const display = document.getElementsByClassName('container')[0];
+          var size = display.offsetWidth
+          if (size >= 720) { // desktop/tablet
+            size /= 2.15; // chess board on one side, list of moves on the other
+          } else if (size <= 575) { // phone
+            size -= 50;
+          }
+          setChessboardSize(size);
+       }, 0) // setTimeout to prevent force reflows (bad for resizing window on low-end systems!)
       }
 
       window.addEventListener('resize', handleResize);
