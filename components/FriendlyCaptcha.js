cfa5214c93699f14742fc654a576977948a95f30
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-08-02 12:39:36 +0000

    add dark mode for frc

--- components/FriendlyCaptcha.js
+++ components/FriendlyCaptcha.js
@@ -1,7 +1,11 @@
-import { useEffect, useRef, forwardRef } from "react";
+// https://www.kindacode.com/article/react-check-if-user-device-is-in-dark-mode-light-mode/
+
+import { useState, useEffect, useRef, forwardRef } from "react";
 import { WidgetInstance } from "friendly-challenge";
 
 const FriendlyCaptcha = ({ sitekey, doneCallback, errorCallback, startMode }, widget) => {
+  const [darkMode, setMode] = useState(false);
+
   const container = useRef();
   const _doneCallback = (solution) => {
     if (doneCallback) doneCallback(solution);
@@ -21,13 +25,32 @@ const FriendlyCaptcha = ({ sitekey, doneCallback, errorCallback, startMode }, wi
       });
     }
 
+    const updateMode = (event) => {
+      const colorScheme = event.matches ? "dark" : "light";
+      console.log(colorScheme); // "dark" or "light"
+      setMode(colorScheme);
+    }
+
+    if (window.matchMedia) {
+      window.matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', updateMode);
+
+      setMode(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    }
+
+
     return () => {
       if (widget.current != undefined) widget.current.destroy();
+
+      if (window.matchMedia) {
+        window.matchMedia('(prefers-color-scheme: dark)')
+          .removeEventListener('change', updateMode)
+      }
     };
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [container]);
 
-  return <div ref={container} className="frc-captcha" data-sitekey={sitekey} />;
+  return <div ref={container} className={darkMode? "frc-captcha dark" : "frc-captcha"} data-sitekey={sitekey} />;
 };
 
 export default forwardRef(FriendlyCaptcha);
