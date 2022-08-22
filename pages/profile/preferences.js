c10084a20be039a2820eeb8ba335e302be6a8766
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-08-22 11:35:15 +0000

    ok!

--- pages/profile/preferences.js
+++ pages/profile/preferences.js
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import Main from "../../components/Main";
 
 export default function Preferences() {
     return (
       <Main title="Preferences">
         <h2>Preferences</h2>
+        <p>test</p>
+        <Image src={"/lol.png"} alt={"image that doesnt exist, for offline placeholder testing"}></Image>
       </Main>
     );
 }
