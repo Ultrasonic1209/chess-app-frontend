f17198ed09e2da91a6b812dc28a1599c83e24ae0
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-08-30 11:51:11 +0000

    darkhouse

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ export default function Checkmate({ Component, pageProps }) {
     <>
         <Head>
           <title>{process.env.appName}</title>
-          <meta name='viewport' content='initial-scale=1.0, maximum-scale=1.0, width=device-width, shrink-to-fit=no, user-scalable=no' />
+          <meta name='viewport' content='initial-scale=1.0, maximum-scale=5.0, width=device-width, shrink-to-fit=no' />
         </Head>
         <Layout>
           <Component {...pageProps} />
