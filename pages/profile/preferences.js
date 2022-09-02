ac327781534d37b50e05036b6d96873a26117c26
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-09-02 22:07:38 +0000

    efewefevf

--- pages/profile/preferences.js
+++ pages/profile/preferences.js
@@ -6,23 +6,22 @@ import { Form, FloatingLabel } from "react-bootstrap";
 
 export default function Preferences() {
 
-  const [boardOrientation, setBoardOrientation] = useLocalStorage("boardOrientation", null);
-  
+  const [boardOrientation, setBoardOrientation] = useLocalStorage("boardOrientation", "1");
+
   return (
     <Main title="Preferences">
       <h2>Preferences</h2>
       <FloatingLabel controlId="orientationSelect" label="Board orientation">
-      <Form.Select
-        aria-label="Which side of the board would you like to be facing?"
-        defaultValue={boardOrientation}
-        onChange={setBoardOrientation}
-      >
-        <option>Choose board orientation</option>
-        <option value={null}>My colour (default)</option>
-        <option value={'white'}>White</option>
-        <option value={'black'}>Black</option>
-      </Form.Select>
-    </FloatingLabel>
+        <Form.Select
+          aria-label="Which side of the board would you like to be facing?"
+          onChange={(ev) => setBoardOrientation(ev.target.value)}
+          value={boardOrientation}
+        >
+          <option value={"1"}>My colour</option>
+          <option value={"2"}>White</option>
+          <option value={"3"}>Black</option>
+        </Form.Select>
+      </FloatingLabel>
       <Image src={"/lol.png"} objectFit={"scale-down"} width={500} height={850} alt={"image that doesnt exist, for offline placeholder testing"}></Image>
     </Main>
   );
