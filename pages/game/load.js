52a7915b7201a921e771df1afdb2d07b23687baa
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-09-13 17:37:07 +0000

    init impl

--- pages/game/load.js
+++ pages/game/load.js
@@ -1,6 +1,6 @@
 import Main from "../../components/Main";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Table, Container, ListGroup, Button } from "react-bootstrap";
 
 import { useRouter } from 'next/router'
@@ -9,11 +9,15 @@ import { useOnlineStatus } from "../../contexts/OnlineStatus";
 
 import { db } from "../../db";
 import { useLiveQuery } from "dexie-react-hooks";
+import useSWR from "swr";
+import { useToastContext } from "../../contexts/ToastContext";
 
 export default function Preferences() {
     const router = useRouter();
 
     const isOnline = useOnlineStatus();
+
+    const addToast = useToastContext()
 
     const [gamemode, setGamemode] = useState();
 
@@ -31,7 +35,25 @@ export default function Preferences() {
       },
     [gamemode]);
 
-    //const remotegames = 
+    const remotePage = useState(0)
+
+    const params = new URLSearchParams({
+      page: remotePage,
+      page_size: 48,
+      my_games: false,
+    });
+
+    const { remotegames, remoteerror } = useSWR(gamemode === "NET" ? "https://apichessapp.server.ultras-playroom.xyz/chess/get-games" + params.toString(): null)
+    
+    useEffect(() => {
+      if (remoteerror) {
+        addToast({
+          "title": "Checkmate",
+          "message": "An error occured whilst retrieving remote games."
+        });
+      }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [remotegames, remoteerror])
 
 
     const onClick = (ev) => {
@@ -66,27 +88,52 @@ export default function Preferences() {
                 </tr>
               </thead>
               <tbody>
-                {games?.map(game => (
-                  <tr key={game.id}>
-                    <td>{game.id}</td>
-                    {
-                      (gamemode === "BOT") || (gamemode === "NET")
-                      ? (<td>
-                        {gamemode === "BOT"
-                        ? "Bot (" + game.difficulty + ")"
+                {
+                  (gamemode === "NET")
+                  ? (
+                    remotegames?.map(game => (
+                    <tr key={game.id}>
+                      <td>{game.id}</td>
+                      {
+                        (gamemode === "BOT") || (gamemode === "NET")
+                        ? (<td>
+                          {gamemode === "BOT"
+                          ? "Bot (" + game.difficulty + ")"
+                          : (undefined)
+                          }
+                        </td>)
                         : (undefined)
-                        }
-                      </td>)
-                      : (undefined)
-                    }
-                    <td>
-                      {game.gameWon ? game.gameWon : "None yet"}
-                    </td>
-                    <td>
-                      <Button variant="primary" data-id={game.id} data-type={game.gameType} onClick={onClick}>Enter Game</Button>
-                    </td>
-                  </tr>
-                ))}
+                      }
+                      <td>
+                        {game.gameWon ? game.gameWon : "None yet"}
+                      </td>
+                      <td>
+                        <Button variant="primary" data-id={game.id} data-type={game.gameType} onClick={onClick}>Enter Game</Button>
+                      </td>
+                    </tr>
+                    )))
+                  : (
+                    games?.map(game => (
+                    <tr key={game.id}>
+                      <td>{game.id}</td>
+                      {
+                        (gamemode === "BOT") || (gamemode === "NET")
+                        ? (<td>
+                          {gamemode === "BOT"
+                          ? "Bot (" + game.difficulty + ")"
+                          : (undefined)
+                          }
+                        </td>)
+                        : (undefined)
+                      }
+                      <td>
+                        {game.gameWon ? game.gameWon : "None yet"}
+                      </td>
+                      <td>
+                        <Button variant="primary" data-id={game.id} data-type={game.gameType} onClick={onClick}>Enter Game</Button>
+                      </td>
+                    </tr>
+                )))}
               </tbody>
             </Table>
             )
