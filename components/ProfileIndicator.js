d537a681a323d841aaf3ab245f99ff438f191df5
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-09-17 13:15:31 +0000

    add offline indicator to profile indicator

--- components/ProfileIndicator.js
+++ components/ProfileIndicator.js
@@ -1,11 +1,15 @@
 // https://nextjs.org/docs/basic-features/data-fetching/client-side
 import Link from 'next/link'
 import useProfile from '../hooks/useProfile'
+import { useOnlineStatus } from '../contexts/OnlineStatus'
 
 export default function ProfileIndicator({onClick}) {
   const { user, error, loading, loggedOut } = useProfile()
+  
+  const isOnline = useOnlineStatus();
 
-  if (error) return <>Signed in as: <Link href="/sign-in" onClick={onClick}>Nobody</Link></>
+  if (!isOnline) return <>Offline</>
+  else if (error) return <>Signed in as: <Link href="/sign-in" onClick={onClick}>Nobody</Link></>
   else if (loading) return <>Loading...</>
   else if (loggedOut) return <>Signed in as: <Link href="/sign-in" onClick={onClick}>Nobody</Link></>
   else return <>Signed in as: <Link href="/profile" onClick={onClick}>{user.name}</Link></>
