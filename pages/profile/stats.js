9b525eef45f497df232fc459c3969c92210be729
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-10-10 21:42:29 +0000

    pklaceholder

--- pages/profile/stats.js
+++ pages/profile/stats.js
@@ -1,9 +1,16 @@
+import useSWR from "swr";
 import Main from "../../components/Main";
+import { fetcher } from "../../hooks/useProfile";
 
 export default function Stats() {
+
+  const { data, error } = useSWR("https://apichessapp.server.ultras-playroom.xyz/user/stats/", fetcher)
+
     return (
       <Main title="Stats">
         <h2>Stats</h2>
+        <p>{data}</p>
+        <b>{error}</b>
       </Main>
     );
 }
