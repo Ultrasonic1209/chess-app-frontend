930ef436fd869d1beffa3b57f96295624fbb28e2
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-10-13 10:04:17 +0000

    MAin

--- pages/docs.js
+++ pages/docs.js
@@ -1,8 +1,11 @@
 import dynamic from "next/dynamic"
 import "swagger-ui-react/swagger-ui.css";
+import Main from "../components/Main";
 
 const SwaggerUI = dynamic(() => import('swagger-ui-react'), {ssr: false});
 
 export default function Docs() {
-    return <SwaggerUI url="https://apichessapp.server.ultras-playroom.xyz/docs/openapi.json" />
+    return <Main title="API Docs">
+        <SwaggerUI url="https://apichessapp.server.ultras-playroom.xyz/docs/openapi.json" />
+    </Main>
 }
