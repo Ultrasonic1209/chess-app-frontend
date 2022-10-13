aa4f79e3f9e98f7122dc980bfd4d5671c4ac91f3
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-10-13 10:28:37 +0000

    fallbacc

--- pages/docs.js
+++ pages/docs.js
@@ -1,11 +1,26 @@
 import dynamic from "next/dynamic"
+import { Suspense } from "react";
 import "swagger-ui-react/swagger-ui.css";
 import Main from "../components/Main";
 
-const SwaggerUI = dynamic(() => import('swagger-ui-react'), {ssr: false});
+const SwaggerUI = dynamic(() => import('swagger-ui-react'), {
+    ssr: false,
+    suspense: true
+});
+
+const fallback = <>
+    <h2>Checkmate API</h2>
+    <p>Swagger is currently loading, please wait...</p>
+</>
 
 export default function Docs() {
     return <Main title="API Docs">
-        <SwaggerUI url="https://apichessapp.server.ultras-playroom.xyz/docs/openapi.json" />
+        <Suspense fallback={fallback}>
+            <SwaggerUI
+                url="https://apichessapp.server.ultras-playroom.xyz/docs/openapi.json"
+                displayRequestDuration={true}
+                tryItOutEnabled={true}
+            />
+        </Suspense>
     </Main>
 }
