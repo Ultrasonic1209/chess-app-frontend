54b479089b39a9b1091a3629237fa272a0ac9fff
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-10-25 16:22:41 +0000

    complete stats??

--- pages/profile/stats.jsx
+++ pages/profile/stats.jsx
@@ -7,28 +7,61 @@ import { useLiveQuery } from "dexie-react-hooks";
 import { useState } from "react";
 
 import { Tab, Tabs } from "react-bootstrap";
+import UserCard from "../../components/UserCard";
+
+function GlobalStats({games_played, games_won, percentage_of_playing_white}) {
+  return <>
+    <p>Games played: {games_played}</p>
+    <p>Games won: {games_won}</p>
+    <p>Percentage of games played as white: {percentage_of_playing_white}</p>
+  </>
+}
 
 export default function Stats() {
 
   const [gamemode, setGamemode] = useState("BOT")
 
-  const { data: netstats, error: neterror } = useSWR(gamemode === "NET" ? "https://apichessapp.server.ultras-playroom.xyz/user/stats/" : undefined, fetcher)
+  const { data: netstats, error: neterror } = useSWR("https://apichessapp.server.ultras-playroom.xyz/user/stats/", fetcher)
+
+  const globalQuery = useLiveQuery(async () => {
+    const amountOfGames = await db.table("games").count();
+
+    const amountOfGamesWon = await db.table("games").filter(function (game) {
+      return game.colourPlaying === game.gameWon
+    }).count();
+
+    const amountOfGamesWhite = await db.table("games").where({
+      colourPlaying: "WHITE"
+    }).count();
+
+    return {
+      amountOfGames,
+      amountOfGamesWhite,
+      amountOfGamesWon
+    }
+  })
 
   const localQuery = useLiveQuery(async () => {
     if ((typeof gamemode === "undefined") || (gamemode === "NET")) { return [] }
 
-    const amountOfGames = await db.table("games").where({
+    const games = db.table("games").where({
       gameType: gamemode
-    }).count();
+    });
 
     const amountOfGamesWhite = await db.table("games").where({
       gameType: gamemode,
       colourPlaying: "WHITE"
     }).count();
 
+    const amountOfGamesWon = await games.filter(function (game) {
+      return game.colourPlaying === game.gameWon
+    }).count();
+
+    const amountOfGames = await games.count()
+
     return {
       games_played: amountOfGames,
-      games_won: 0,
+      games_won: amountOfGamesWon,
       percentage_of_starting_white: ((amountOfGamesWhite / amountOfGames) * 100) || 0
     }
   }, [gamemode]);
@@ -37,7 +70,23 @@ export default function Stats() {
     <Main title="Stats">
       <h2>Stats</h2>
       <h3>Global stats</h3>
-      <p>we&rsquo;ll... be right back!</p>
+      {(globalQuery && netstats)
+      ? <GlobalStats
+          games_played={globalQuery.amountOfGames + netstats.games_played + ' (' + netstats.games_played + ' online)'}
+          games_won={globalQuery.amountOfGamesWon + netstats.games_won + ' (' + netstats.games_won + ' online)'}
+          percentage_of_playing_white={(((globalQuery.amountOfGamesWhite / globalQuery.amountOfGames) * 100) || 0).toFixed(2) + '% offline, ' + (netstats.percentage_of_playing_white || 0).toFixed(2) + '% online'}
+        />
+      : ((globalQuery && neterror)
+          ? <>
+              <b>Online game stats are not available.</b>
+              <GlobalStats
+                games_played={globalQuery.amountOfGames}
+                games_won={globalQuery.gamesWon}
+                percentage_of_playing_white={(((globalQuery.amountOfGamesWhite / globalQuery.amountOfGames) * 100) || 0).toFixed(2) + '% offline'}
+              />
+            </>
+          : <b>Loading</b>
+      )}
       <h3>Category stats</h3>
       <Tabs
         id="controlled-tab-example"
@@ -57,17 +106,37 @@ export default function Stats() {
         </Tab>
         <Tab eventKey="LOCAL" title="Local">
           {localQuery && (gamemode != "NET")
-              ? <>
-                <p>Games played: {localQuery.games_played}</p>
-                <p>Games won: {localQuery.games_won} ({(((localQuery.games_won / localQuery.games_played) || 0) * 100).toFixed(2)}%)</p>
-                <p>Percentage of games played as white: {(localQuery.percentage_of_starting_white || 0).toFixed(2)}%</p>
-              </>
-              : <b>Loading</b>
-            }
+            ? <>
+              <p>Games played: {localQuery.games_played}</p>
+              <p>Games won: {localQuery.games_won} ({(((localQuery.games_won / localQuery.games_played) || 0) * 100).toFixed(2)}%)</p>
+              <p>Percentage of games played as white: {(localQuery.percentage_of_starting_white || 0).toFixed(2)}%</p>
+            </>
+            : <b>Loading</b>
+          }
         </Tab>
         <Tab eventKey="NET" title="Online">
-          <p>{JSON.stringify(netstats)}</p>
-          <b>{JSON.stringify(neterror)}</b>
+          {netstats && (gamemode === "NET")
+            ? <>
+              <p>Games played: {netstats.games_played}</p>
+              <p>Games won: {netstats.games_won} ({(((netstats.games_won / netstats.games_played) || 0) * 100).toFixed(2)}%)</p>
+              <p>Percentage of games played as white: {(netstats.percentage_of_playing_white || 0).toFixed(2)}%</p>
+              <p className={"mb-0"}>Favourite opponent:</p> {
+                netstats.favourite_opponent
+                  ? <UserCard
+                    username={netstats.favourite_opponent.name || "Anonymous"}
+                    avatarhash={netstats.favourite_opponent.avatar_hash}
+                    rank={netstats.favourite_opponent.rank}
+                  />
+                  : <b>No opponents?</b>
+                }
+            </>
+            : (neterror
+              ? <>
+                <p>Online game stats are not available.</p>
+              </>
+              : <b>Loading</b>
+            )
+          }
         </Tab>
       </Tabs>
     </Main>
