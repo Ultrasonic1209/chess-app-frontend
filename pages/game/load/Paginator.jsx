75adf340956f9276e02e98a6fbf9808d6a3cd15b
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2022-11-03 12:49:18 +0000

    im SO SMARRRT

--- pages/game/load/Paginator.jsx
+++ pages/game/load/Paginator.jsx
@@ -1,10 +1,11 @@
 import { Pagination } from "react-bootstrap";
 
 export default function Paginator(page, setPage) {
-    <Pagination className="p-0 pt-3">
-    <Pagination.First data-page={"1"} onClick={setPage} disabled={(page || "1") === "1"} />
-    <Pagination.Prev data-page={(parseInt(page) || 2) - 1} onClick={setPage} disabled={(page || "1") === "1"} />
-    <Pagination.Item active hr>{parseInt(page) || 1}</Pagination.Item>
-    <Pagination.Next data-page={(parseInt(page) || 1) + 1} onClick={setPage} disabled={setPage < 1} />
-  </Pagination>
+
+    return <Pagination className="p-0 pt-3">
+        <Pagination.First data-page={"1"} onClick={setPage} disabled={(page || "1") === "1"} />
+        <Pagination.Prev data-page={(parseInt(page) || 2) - 1} onClick={setPage} disabled={(page || "1") === "1"} />
+        <Pagination.Item active hr>{parseInt(page) || 1}</Pagination.Item>
+        <Pagination.Next data-page={(parseInt(page) || 1) + 1} onClick={setPage} disabled={setPage < 1} />
+    </Pagination>
 }
